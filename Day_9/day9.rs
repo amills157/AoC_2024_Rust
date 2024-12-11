81e220339fac43104a40d982c6be73279755931d
amills157 <alanmills88@gmail.com>
2024-12-11 14:02:32 +0000

    Day9 - Pt1 complete

--- Day_9/day9.rs
+++ Day_9/day9.rs
@@ -1,6 +1,8 @@
 use std::io::prelude::*;
 use std::fs::File;
 use std::collections::BTreeMap;
+use std::iter::FromIterator;
+
 
 fn read_example_txt() -> String{
     let mut file = File::open("example.txt").expect("Unable to open the file");
@@ -10,7 +12,8 @@ fn read_example_txt() -> String{
     return contents;
 }
 
-fn string_disk_space_representation(input: &str) -> (Vec<char>, usize) {
+// This is ugly - But it works. Multiple returns make it easier to keep track of files v free space
+fn string_disk_space_representation(input: &str) -> (Vec<String>, Vec<usize>, usize) {
 
     let char_vec: Vec<char> = input.chars().collect();
 
@@ -19,19 +22,20 @@ fn string_disk_space_representation(input: &str) -> (Vec<char>, usize) {
     let mut free_space_count = 0;
 
     let mut vec = Vec::new();
+    
+    let mut usize_vec = Vec::new();
 
-    // This is ugly but using as to cast returns the ascii to int, rather than char to value as int
     for i in 0..char_vec.len() {
         if i % 2 == 0{
 
-            // println!("index = {}", i);
-
-            // println!("char = {}", char_vec[i]);
-
             let multipler = char_vec[i].to_string().parse::<usize>().unwrap();
-
-            for j in 0..multipler{
-                vec.push(char::from_digit(id as u32, 10).unwrap())
+            
+            if multipler != 0{
+                let id_str: String = id.to_string();
+                for j in 0..multipler{
+                    vec.push(id_str.clone());
+                    usize_vec.push(id);
+                }
             }
 
             id += 1;
@@ -42,72 +46,55 @@ fn string_disk_space_representation(input: &str) -> (Vec<char>, usize) {
                 let multipler = char_vec[i].to_string().parse::<usize>().unwrap();
 
                 for j in 0..multipler{
-                    vec.push('.');
+                    vec.push(".".to_string());
                     free_space_count += 1;
                 }
 
             }
         }
 
-        //println!("{:?}", vec);
     }
 
-    return (vec, free_space_count);
+    return (vec, usize_vec, free_space_count);
 
 }
 
-
+#[allow(dead_code, unused_variables)]
 fn part_one(){
     let string_value = read_example_txt();
 
-    let (mut disk_space_vec, mut free_space_count) = string_disk_space_representation(&string_value);
+    let (mut disk_space_vec, mut files_only, mut free_space_count) = string_disk_space_representation(&string_value);
     
-    println!("{:?}", disk_space_vec);
+    //println!("{:?}", disk_space_vec);
 
     let mut last_element_tracker = 1;
+    
+    let mut sorted_disk_space_vec = Vec::new();
 
-    // TODO: Probably easier to create a new array rather than try and be smart here
     for i in 0..disk_space_vec.len(){
-        if disk_space_vec[i] == '.'{
-            disk_space_vec[i] = disk_space_vec[disk_space_vec.len() - last_element_tracker];
-            disk_space_vec.push('.');
-            disk_space_vec.remove(disk_space_vec.len() - last_element_tracker);
+        if disk_space_vec[i] == "."{
+            let usize_to_push = files_only[files_only.len() - last_element_tracker];
+            sorted_disk_space_vec.push(usize_to_push);
             last_element_tracker += 1;
-            free_space_count -= 1;
-
-            println!("free space count = {:?}", free_space_count);
-
-            if free_space_count == 0{
-                break;
-            }
                 
+        } else {
+            sorted_disk_space_vec.push(disk_space_vec[i].to_string().parse::<usize>().unwrap());
         }
-        println!("{:?}", disk_space_vec);
+        
+        //println!("{:?}", sorted_disk_space_vec);
+        
+        if (sorted_disk_space_vec.len() + free_space_count) == disk_space_vec.len(){
+            break;
+        }
+    }
+    
+    let mut sum = 0;
+    
+    for i in 0..sorted_disk_space_vec.len(){
+        sum += i * sorted_disk_space_vec[i]
     }
 
-    println!("{:?}", disk_space_vec);
-
-    
-    // let (mut int_vec_1, mut int_vec_2) = string_to_int_vectors(&string_value);
-
-    // println!("{:?}", files);
-
-    // println!("{:?}", free_space);
-
-    // let mut sum = 0;
-
-    // for (pos, e) in int_vec_1.iter().enumerate() {
-    //     // println!("Element at position {}: {:?}", pos, e);
-
-    //     let diff = (e - int_vec_2[pos]).abs();
-
-    //     sum += diff;
-
-    //     //println!("{}", diff);
-
-    // }
-
-    // println!("The answer to part one is {}", sum);
+    println!("The answer to part one is {}", sum);
 }
 
 
@@ -135,7 +122,7 @@ fn part_one(){
 
 // }
 
-
+// https://www.onlinegdb.com/online_rust_compiler
 fn main() {    
     part_one();
 
